f6336c237a9693ae0d6bc35b5bfeda60a1d3bffd
vianney diris <vianney.diris@gmail.com>
2020-04-02 18:30:17 +0200

    add basic function of angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-project';
+  title = 'Angular Training';
+  firstName = 'Vianney';
+  lastName = 'Diris';
+
+  values = ['ceci','est','une','boucle','ngFor','avec un ngIf'];
+  show=true;
+
+  doClick(){
+    this.title = "clique sur bouton";
+  }
+
+  toggle(){
+    this.show = this.show?false:true;
+  }
 }
